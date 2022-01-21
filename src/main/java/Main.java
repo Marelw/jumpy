3e2b5765715025680817ce9b6965d42c753b31e2
gabqui <gabqui0405@skola.goteborg.se>
2022-01-21 11:29:14 +0100

    Coding the JFrame for the game.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        JFrame f = new JFrame();
         
+        f.setTitle("Flappy Birb");
+        f.setVisible(true);
+
+        // Window size and location
+        f.setSize(400, 500);
+        f.setLocation(100,100);
+        f.setResizable(true);
+        
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
