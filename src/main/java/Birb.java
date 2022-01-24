aafd82262cf8f1cb5b2bb0b34db1bdf660945627
Oscar <oscnor0930@skola.goteborg.se>
2022-01-24 10:43:41 +0100

    birb changes

--- src/main/java/Birb.java
+++ src/main/java/Birb.java
@@ -5,6 +5,7 @@ import java.awt.Toolkit;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
+
 public class Birb {
 
     /**
@@ -12,49 +13,23 @@ public class Birb {
      */
     private boolean isAlive = true;
     private Image jumpyBirb;
+    public double velocity;
+    public double gravity;
+    private int keyResponseDeley;
+    private double birdShift;
+
 
     /**
      * Bird constants
      */
     public final int BIRD_HEIGHT = 25;
     public final int BIRD_WIDTH = 39;
-    private final int STARTING_BIRD_X_LOC = 80;
-    private final int STARING_BIRD_Y_LOC = 320;
+    private final int STARTING_BIRD_X_LOC = 100;
+    private final int STARING_BIRD_Y_LOC = 150;
+    private Image image;
+    private Keyboard keyboard; // bryt ut Keyboard response till egen klass
 
-    /**
-     * Default bird constructor
-     * @return 
-     */
-    public Birb(int birdWidth, int birdHeight) {
-        jumpyBirb = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("lib/bluejumpy.png"));
-        scaleBird(birdWidth, birdHeight);
-    }
-
-    /**
-     * Method to scale bird
-     */
-    public void scaleBird(int width, int height) {
-        jumpyBirb = jumpyBirb.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-    }
-
-    /**
-     * Method to get rectangle that outlines the bird's image
-     * @return rectangle outline of bird on screen
-     */
-    public Rectangle getRectangle() {
-        return (new Rectangle(STARTING_BIRD_X_LOC, STARING_BIRD_Y_LOC, jumpyBirb.getWidth(null), jumpyBirb.getHeight(null)));
-    }
-
-    /**
-     * Method to get a buffered image that represents the bird object
-     * @return bird bufferdd image object
-     */
-    public BufferedImage getBufferedImage() {
-        BufferedImage bi = new BufferedImage(jumpyBirb.getWidth(null), jumpyBirb.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-        Graphics g = bi.getGraphics();
-        g.drawImage(jumpyBirb, 0, 0, null);
-        g.dispose();
-        return bi;
+    public Birb() {
     }
 
     /**
