a5987dcdc663d2ea95626cb0b061611cae807451
Oscar <oscnor0930@skola.goteborg.se>
2022-01-28 10:27:17 +0100

    created new birbclass, modify keyboard, adding new bluejumpy

--- src/main/java/Birb.java
+++ src/main/java/Birb.java
@@ -2,7 +2,7 @@ import java.awt.Graphics;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import java.awt.image.BufferedImage;
 
 public class Birb extends JFrame {
 
@@ -19,28 +19,30 @@ public class Birb extends JFrame {
     /**
      * Bird constants
      */
-    public final int BIRD_HEIGHT = 100;
-    public final int BIRD_WIDTH = 150;
-    private int STARTING_BIRD_X_LOC = 100;
-    private int STARING_BIRD_Y_LOC = 150;
+    public final int BIRB_HEIGHT = 100;
+    public final int BIRB_WIDTH = 150;
+    private int STARTING_BIRB_X_LOC = 100;
+    private int STARING_BIRB_Y_LOC = 150;
     private JLabel blueJumpy;
 
     
     private ImageIcon image;
     //private Keyboard keyboard; // bryt ut Keyboard response till egen
-    
+
+
+
     public Birb(ImageIcon image, int x, int y) {
-        this.STARTING_BIRD_X_LOC = x;
-        this.STARING_BIRD_Y_LOC = y;
+        this.STARTING_BIRB_X_LOC = x;
+        this.STARING_BIRB_Y_LOC = y;
         this.image = image;
         setupBirb();
     }
 
-    public int getSTARTING_BIRD_X_LOC() {
-        return STARTING_BIRD_X_LOC;
+    public int getSTARTING_BIRB_X_LOC() {
+        return STARTING_BIRB_X_LOC;
     }
-    public int getSTARING_BIRD_Y_LOC() {
-        return STARING_BIRD_Y_LOC;
+    public int getSTARING_BIRB_Y_LOC() {
+        return STARING_BIRB_Y_LOC;
     }
 
     public JLabel getBirb(){
@@ -49,7 +51,7 @@ public class Birb extends JFrame {
 
     public void setupBirb(){
         blueJumpy = new JLabel();
-        blueJumpy.setBounds(this.STARTING_BIRD_X_LOC, this.STARING_BIRD_Y_LOC, BIRD_WIDTH, BIRD_HEIGHT);
+        blueJumpy.setBounds(this.STARTING_BIRB_X_LOC, this.STARING_BIRB_Y_LOC, BIRB_WIDTH, BIRB_HEIGHT);
         blueJumpy.setIcon(this.image);
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,27 +8,26 @@ public class Main {
     }
 
     private static void GameDisplay() {
-        ImageIcon icon = new ImageIcon(new ImageIcon("lib/background.png").getImage());
-        
-        JLabel label = new JLabel(icon);
         JFrame frame = new JFrame("Flappy Birb");
-        JPanel panel = new JPanel();
-        
-        ImageIcon image = new ImageIcon("lib/bluejumpy.png");
-        Birb birb = new Birb(image, 200, 100);
-        frame.getContentPane().add(birb.getBirb());
-
-        frame.add(panel);
-        panel.add(label);
-        frame.setVisible(true);
-        
-        // Window size and location
-        //frame.setSize(400, 500);
-        //frame.setLocation(100,100);
+        frame.setBounds(100, 100, 500, 500);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        frame.setBounds(100, 100, 800, 500);
-
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImageIcon icon = new ImageIcon(new ImageIcon("lib/background.png").getImage());
+        //ImageIcon image = new ImageIcon("lib/bluejumpy.png");
+        BirbTwo birb = new BirbTwo();
+        //frame.getContentPane().add(birb.getBirb());
+
+        JLabel label = new JLabel(icon);
+        JPanel panel = new JPanel();
+        //frame.add(panel);
+        panel.add(label);
+
+        frame.add(birb);
+
+
+
+        frame.setVisible(true);
+
     }
 }
