8e3b1ae360196459213dddae5bf12c617b9ea619
Oscar <oscnor0930@skola.goteborg.se>
2022-02-18 08:16:09 +0100

    Changing GC, adding MyFrame class, removing Keyboard class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,6 @@
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
-import javax.swing.JFrame;
-import java.awt.*;
-
-
 public class Main {
     public static void main(String[] args) {
-        GameDisplay();
-    }
 
-    private static void GameDisplay() {
-        JFrame frame = new JFrame("Jumpy Birb");
-        frame.setSize(650, 650);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        GameConstraints gC = new GameConstraints();
-        frame.add(gC);
-
-
-
-        frame.setVisible(true);
+        new MyFrame();
     }
 }
