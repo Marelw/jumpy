714896b77e7b30764cc6ad340f71d78d0e52869d
Marelw <marelw1112@skola.goteborg.se>
2022-03-07 12:44:18 +0100

    Reverting Main class, adding menu in Gameconstraints and adding feature in MyFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-    public static String playerName;
     public static void main(String[] args)  {
-        try {
-            gameMenu();
-        } catch (InterruptedException ex){
-            System.err.println("Error loading the gamemenu");
-            ex.printStackTrace();
-        }
-
-    }
-
-    private static void gameMenu() throws InterruptedException {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome");
-        System.out.println("Before we begin i would like to know your name");
-        System.out.println("What should i call you?");
-        playerName = scan.nextLine();
-        System.out.println("Okay " + playerName + ", lets see what you got!");
-        System.out.println("Enjoy your game!");
-        Thread.sleep(2000);
         new MyFrame();
     }
 }
